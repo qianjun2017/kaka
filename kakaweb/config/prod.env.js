473f84537711f0a8ef43f5f0daf92b6101ea4213
qianjun2017 <ws_yuan@126.com>
2019-04-14 15:04:25 +0800

    build

--- kakaweb/config/prod.env.js
+++ kakaweb/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_URL: '"https://api.ccyws.cn"',
+  BASE_URL: '"https://api.shiweiinfo.com"',
   FILE_URL: '"https://file.ccyws.cn"'
 }
