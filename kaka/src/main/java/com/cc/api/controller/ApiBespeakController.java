b8b974c7291b8014bd780d1b3dceca46141f7ec8
qianjun2017 <ws_yuan@126.com>
2019-04-14 21:00:38 +0800

    预约看车

---
+++ kaka/src/main/java/com/cc/api/controller/ApiBespeakController.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package com.cc.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cc.api.form.BespeakForm;
+import com.cc.common.exception.LogicException;
+import com.cc.common.web.RequestContextUtil;
+import com.cc.common.web.Response;
+import com.cc.consultant.bean.ConsultantBean;
+import com.cc.customer.bean.BespeakBean;
+import com.cc.customer.service.BespeakService;
+import com.cc.franchiser.bean.FranchiserBean;
+
+/**
+ * @author Administrator
+ *
+ */
+@Controller
+@RequestMapping("/api/bespeak")
+public class ApiBespeakController {
+
+	@Autowired
+	private BespeakService bespeakService;
+	
+	/**
+	 * 预约看车
+	 * @param form
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public Response<String> addBespeak(@RequestBody BespeakForm form){
+		Response<String> response = new Response<String>();
+		BespeakBean bespeakBean = new BespeakBean();
+		bespeakBean.setCustomerId(RequestContextUtil.getCustomerId());
+		if(form.getConsultantId()==null){
+			response.setMessage("请选择汽车顾问");
+			return response;
+		}
+		ConsultantBean consultantBean = ConsultantBean.get(ConsultantBean.class, form.getConsultantId());
+		if(consultantBean==null){
+			response.setMessage("汽车顾问不存在，请重新选择");
+			return response;
+		}
+		bespeakBean.setConsultantId(form.getConsultantId());
+		if(form.getFranchiserId()==null){
+			response.setMessage("请选择经销商");
+			return response;
+		}
+		FranchiserBean franchiserBean = FranchiserBean.get(FranchiserBean.class, form.getFranchiserId());
+		if(franchiserBean==null){
+			response.setMessage("经销商不存在，请重新选择");
+			return response;
+		}
+		bespeakBean.setFranchiserId(form.getFranchiserId());
+		if(form.getBespeakTime()==null){
+			response.setMessage("请选择看车时间");
+			return response;
+		}
+		bespeakBean.setBespeakTime(form.getBespeakTime());
+		try {
+			bespeakService.saveBespeak(bespeakBean);
+			response.setSuccess(Boolean.TRUE);
+		} catch (LogicException e) {
+			response.setMessage(e.getErrContent());
+		} catch (Exception e) {
+			response.setMessage("系统内部错误");
+			e.printStackTrace();
+		}
+		return response;
+	}
+}
---
+++ kaka/src/main/java/com/cc/api/form/BespeakForm.java
@@ -0,0 +1,63 @@
+package com.cc.api.form;
+
+import java.util.Date;
+
+public class BespeakForm {
+
+	/**
+	 * 看车时间
+	 */
+	private Date bespeakTime;
+	
+	/**
+	 * 经销商
+	 */
+	private Long franchiserId;
+	
+	/**
+	 * 汽车顾问
+	 */
+	private Long consultantId;
+
+	/**
+	 * @return the bespeakTime
+	 */
+	public Date getBespeakTime() {
+		return bespeakTime;
+	}
+
+	/**
+	 * @param bespeakTime the bespeakTime to set
+	 */
+	public void setBespeakTime(Date bespeakTime) {
+		this.bespeakTime = bespeakTime;
+	}
+
+	/**
+	 * @return the franchiserId
+	 */
+	public Long getFranchiserId() {
+		return franchiserId;
+	}
+
+	/**
+	 * @param franchiserId the franchiserId to set
+	 */
+	public void setFranchiserId(Long franchiserId) {
+		this.franchiserId = franchiserId;
+	}
+
+	/**
+	 * @return the consultantId
+	 */
+	public Long getConsultantId() {
+		return consultantId;
+	}
+
+	/**
+	 * @param consultantId the consultantId to set
+	 */
+	public void setConsultantId(Long consultantId) {
+		this.consultantId = consultantId;
+	}
+}
